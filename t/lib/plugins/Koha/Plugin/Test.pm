6458ec860c55b4be0b5f5614ad43656989a41ccf
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2022-10-17 16:48:58 +0200

    Bug 31897: New before_index_action hook for Elasticsearch
    Hooks added:
    before_index_action with new action update
    
    How to test:
    Run tests in t/db_dependent/Koha/Plugins/Elasticsearch_Indexer_hooks.t
    
    Sponsored by: Gothenburg University Library
    
    Signed-off-by: David Nind <david@davidnind.com>

--- t/lib/plugins/Koha/Plugin/Test.pm
+++ t/lib/plugins/Koha/Plugin/Test.pm
@@ -492,6 +492,17 @@ sub overwrite_calc_fine {
     return [ 1, 2, 3 ];
 }
 
+sub before_index_action {
+    my ( $self, $params ) = @_;
+
+    my $action = $params->{action};
+    my $engine   = $params->{payload}->{engine};
+    my $records   = $params->{payload}->{records};
+
+    Koha::Exception->throw(
+        "before_index_action called with action: $action, engine: $engine, ref: " . ref($records) );
+}
+
 sub _private_sub {
     return "";
 }
