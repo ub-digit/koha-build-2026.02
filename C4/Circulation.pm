740ad8c999a9202b32fe36fdc64687889898c41a
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2022-10-27 09:22:56 +0200

    Bug 36303: Add after_circ_action hook action for checking in item that was not checked out
    To test:
    
    1) Run tests in t/db_dependent/Koha/Plugins/Circulation_hooks.t
    
    Sponsored-by: Gothenburg University Library

--- C4/Circulation.pm
+++ C4/Circulation.pm
@@ -2751,6 +2751,14 @@ sub AddReturn {
 
     }
 
+    if ( !$issue ) {
+        Koha::Plugins->call( 'after_circ_action', {
+            action  => 'checkin_no_issue',
+            payload => {
+                checkin => $item
+            }
+        });
+    }
     return ( $doreturn, $messages, $issue, ( $patron ? $patron->unblessed : {} ) );
 }
 
