6f397715cdd566c68b0a54566f0d8c4f9ed8549b
Per Larsson <per.larsson@ub.gu.se>
2023-03-01 13:23:12 +0100

    Always show circulation settings on checkout view

--- koha-tmpl/intranet-tmpl/prog/js/pages/circulation.js
+++ koha-tmpl/intranet-tmpl/prog/js/pages/circulation.js
@@ -70,6 +70,8 @@ $(document).ready(function () {
         }
     });
 
+    $("#checkout-circ-settings").show();
+
     $(".circ_setting").on("click", function () {
         $("#barcode").focus();
     });
