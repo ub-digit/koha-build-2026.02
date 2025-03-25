de40a7dfa32e80e96f7132fd0a3e58422a8e96ab
David Gustafsson <david.gustafsson@ub.gu.se>
2025-03-25 21:28:12 +0100

    Bug 39453: Add attribute type settings for restricting access to extended attributes
    Add new attribute type settings "readonly", "secret" and "hidden" for
    patron attribute types.
    
    All restrictions enforced by these new settings are not applied for superlibrarians, where attributes behave as if they have not been set.
    
    Readonly make the attributes of this type readonly, they appear on the patrons details page and when editing a patron, but their values can't be changed.
    
    Hidden attributes are not visible on the patrons details page or when editing a patron, but can for example be used internally.
    
    Secret attributes are basically the same thing as hidden, but the appear with their value masked in the patrons details page.
    
    To Test:
    1) Apply patch
    3) Three new attribute type, each with one of the new settings set.
    4) In addition to this create an attribute type which is a date, and set
       this to readonly.
    5) As a superlibrarian, edit a patron and set values for the attributes of these types6) Log in as a non superlibrarian. Go to the same patron and verify that:
    7) The attributes that are readonly can be viewd in the patrons details
       page, and when editing the patron, but their value can't be changed.
    8) The hidden attribute should appear nowhere.
    9) The secret attribute should be visible in the patrons details page,
       but masked. It should not appear when editing the patron.
    10) Edit the patrons extended patron and save.
    11) Log in as a superlibrarian, go to the patrons details page and
        verify the extended attribute values remains the same.
    
    Sponsored-by: Gothenburg University Library

--- admin/patron-attr-types.pl
+++ admin/patron-attr-types.pl
@@ -109,6 +109,9 @@ sub add_update_attribute_type {
     my $description               = $input->param('description');
     my $repeatable                = $input->param('repeatable')                ? 1 : 0;
     my $unique_id                 = $input->param('unique_id')                 ? 1 : 0;
+    my $hidden                    = $input->param('hidden')                    ? 1 : 0;
+    my $readonly                  = $input->param('readonly')                  ? 1 : 0;
+    my $secret                    = $input->param('secret')                    ? 1 : 0;
     my $is_date                   = $input->param('is_date')                   ? 1 : 0;
     my $opac_display              = $input->param('opac_display')              ? 1 : 0;
     my $opac_editable             = $input->param('opac_editable')             ? 1 : 0;
@@ -146,6 +149,9 @@ sub add_update_attribute_type {
         {
             repeatable                => $repeatable,
             unique_id                 => $unique_id,
+            hidden                    => $hidden,
+            readonly                  => $readonly,
+            secret                    => $secret,
             is_date                   => $is_date,
             opac_display              => $opac_display,
             opac_editable             => $opac_editable,
