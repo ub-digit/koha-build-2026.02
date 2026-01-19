80c61fae1dbdc0683f80644bb1d178a939ff5e65
David Gustafsson <david.gustafsson@ub.gu.se>
2026-01-19 10:57:58 +0100

    Add weeks argument to clean_deleted_borrowers.pl

--- misc/cronjobs/clean_deleted_borrowers.pl
+++ misc/cronjobs/clean_deleted_borrowers.pl
@@ -8,7 +8,13 @@ use C4::Context;
 use Koha::Patrons;
 use Modern::Perl;
 
+my $weeks = shift @ARGV
+    or die "Usage: $0 <weeks>\n";
+
+die "Weeks must be a positive integer\n"
+    unless $weeks =~ /^\d+$/;
+
 my $dbh = C4::Context->dbh();
 
-my $sth = $dbh->prepare("DELETE FROM deletedborrowers WHERE updated_on < DATE_SUB(NOW(), INTERVAL 1 WEEK);");
-my $res = $sth->execute();
+my $sth = $dbh->prepare("DELETE FROM deletedborrowers WHERE updated_on < DATE_SUB(NOW(), INTERVAL ? WEEK);");
+my $res = $sth->execute($weeks);
