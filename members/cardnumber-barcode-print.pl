bea6f0818e07b88541f5f9a6858336850471a144
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2026-02-11 15:24:43 +0100

    Implement printing of barcode for patron in the Print menu

---
+++ members/cardnumber-barcode-print.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/perl
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <https://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use CGI;
+
+use C4::Auth   qw( get_template_and_user );
+use C4::Output qw( output_and_exit_if_error output_and_exit output_html_with_http_headers );
+use C4::Members;
+use Koha::Patrons;
+
+my $input          = CGI->new;
+my $borrowernumber = $input->param('borrowernumber');
+
+my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
+    {
+        template_name => "members/moremember-barcode.tt",
+        query         => $input,
+        type          => "intranet",
+        flagsrequired => { circulate => "circulate_remaining_permissions" },
+    }
+);
+
+my $logged_in_user = Koha::Patrons->find($loggedinuser);
+my $patron         = Koha::Patrons->find($borrowernumber);
+output_and_exit_if_error(
+    $input, $cookie, $template,
+    { module => 'members', logged_in_user => $logged_in_user, current_patron => $patron }
+);
+
+my $barcode_format = C4::Context->preference('OPACVirtualCardBarcode');
+
+$template->param(
+    patron => $patron,
+    barcode_format => $barcode_format,
+);
+
+output_html_with_http_headers $input, $cookie, $template->output;
