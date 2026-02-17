ub-digit/Koha-build

Add a simplified messaging functionallity option
added syspref for enabling the simplified messaging option in opac

added code to simulate the queryparams if simplifeid messaging is enabled

added the simplifed form for messaging. Hiding the original form also

Intranet is now using the simplified version of the messaging preferences

added the simplifed form for messaging. Hiding the original form also

removed blank lines

added new syspref and using it to set the correct notice methods for listed events

added ticking the methods based on syspref

added syspref for enabling the simplified messaging option in opac

added code to simulate the queryparams if simplifeid messaging is enabled

added the simplifed form for messaging. Hiding the original form also

added syspref for enabling the simplified messaging option in opac

added the simplifed form for messaging. Hiding the original form also

Intranet is now using the simplified version of the messaging preferences

removed blank lines

added new syspref and using it to set the correct notice methods for listed events

added ticking the methods based on syspref

conflicts removed

moved code for handeling simplified messaging to hande both insert and update

removed conflict

added latest code from master

added code from master

Text changes

added support for days-in-advanced

added info about email and smsnumber on messaging page

moved code to correct place

added smsalertnumber to edit-form

changed copy

added id to link for scrolling to correct place

added classes for enabling translations

moved span-tag

email is now e-mail in copy

changed label for smsnumber

Do not update smsalertnumber and email when simplified messaging is enabled

Fixed typo

Remove custom GUB properties before store

Get SMSSendDriver value from preference

checks that patron has a registred sms-number before allowing save when sms or sms/email is selected. KOHA-1524

checks that SMS-number has been entered before allowing sms 

members/memberentry.pl
@@ -31,6 +31,7 @@ use C4::Output qw( output_and_exit output_and_exit_if_error output_html_with_htt
 use C4::Members qw( checkcardnumber get_cardnumber_length );
 use C4::Koha qw( GetAuthorisedValues );
 use C4::Letters qw( SendAlerts );
+use C4::Members::Messaging;
 use C4::Form::MessagingPreferences;
 use Koha::AuthUtils;
 use Koha::AuthorisedValues;
@@ -46,6 +47,8 @@ use Koha::Patron::HouseboundRoles;
 use Koha::Token;
 use Email::Valid;
 use Koha::SMS::Providers;
+
+my $messaging_options = C4::Members::Messaging::GetMessagingOptions();
 
 my $input = CGI->new;
 my %data;
@@ -420,6 +423,57 @@ if ( ($op eq 'modify' || $op eq 'insert' || $op eq 'save'|| $op eq 'duplicate') 
     unless ($newdata{'dateexpiry'}){
         my $patron_category = Koha::Patron::Categories->find( $newdata{categorycode} );
         $newdata{'dateexpiry'} = $patron_category->get_expiry_date( $newdata{dateenrolled} ) if $patron_category;
+    }
+}
+
+# do this for both save and insert  if EnhancedMessagingPreferences and simple-messaging is used
+if ( $op eq 'insert' || $op eq 'save') {
+    # if simplified form is to be used we add the params here
+    if (C4::Context->preference('EnhancedMessagingPreferences') and $input->param('setting_messaging_prefs')) {
+      if ( defined $input->param('simple-messaging') && $input->param('simple-messaging') eq 'yes') {
+          if (defined $input->param ('opacmessaging-simple-radios')) {
+              my %whichActionsToTickUsingSimpleOpacMessaging = map { $_ => 1 } (split /\|/, C4::Context->preference('whichActionsToTickUsingSimpleOpacMessaging')); # split the string to array and then convert to hash to use keys for easy checking
+              if ($input->param('opacmessaging-simple-radios') eq 'sms') {
+                  foreach my $messaging_option (@{$messaging_options})
+                  {
+                    if ($whichActionsToTickUsingSimpleOpacMessaging{$messaging_option->{'message_name'}}) {
+                        $input->param($messaging_option->{'message_attribute_id'}, "sms");
+
+                    }
+                  }
+
+              }
+              elsif ($input->param('opacmessaging-simple-radios') eq 'email') {
+                  # set all types to email
+                  foreach my $messaging_option (@{$messaging_options})
+                  {
+                    if ($whichActionsToTickUsingSimpleOpacMessaging{$messaging_option->{'message_name'}}) {
+                         $input->param($messaging_option->{'message_attribute_id'}, "email");
+                    }
+                  }
+              }
+              elsif (($input->param('opacmessaging-simple-radios') eq 'SmsAndEmail')) {
+                   # set all types to email and sms
+                  foreach my $messaging_option (@{$messaging_options})
+                  {
+                    if ($whichActionsToTickUsingSimpleOpacMessaging{$messaging_option->{'message_name'}}) {
+                      $input->param($messaging_option->{'message_attribute_id'}, "sms", "email");
+                    }
+                  }
+              }
+              elsif (($input->param('opacmessaging-simple-radios') eq 'paper')) {
+                   # set all types to do not notify
+                  foreach my $messaging_option (@{$messaging_options})
+                  {
+                    if ($whichActionsToTickUsingSimpleOpacMessaging{$messaging_option->{'message_name'}}) {
+                      $input->delete($messaging_option->{'message_attribute_id'});
+                    }
+                  }
+              }
+          }
+      }
+      delete $newdata{"opacmessaging-simple-radios"};
+      delete $newdata{"simple-messaging"};
     }
 }
 

ub-digit/koha-build-2026.02

members/memberentry.pl
@@ -30,7 +30,8 @@ use Scalar::Util qw( blessed );
 use C4::Auth qw( get_template_and_user haspermission );
 use C4::Context;
 use C4::Output  qw( output_and_exit output_and_exit_if_error output_html_with_http_headers );
-use C4::Letters qw( GetPreparedLetter EnqueueLetter SendQueuedMessages );
+use C4::Letters qw( GetPreparedLetter EnqueueLetter SendQueuedMessages SendAlerts);
+use C4::Members::Messaging;
 use C4::Form::MessagingPreferences;
 use Koha::AuthUtils;
 use Koha::AuthorisedValues;
@@ -48,6 +49,8 @@ use Koha::Patron::HouseboundRoles;
 use Koha::Policy::Patrons::Cardnumber;
 use Koha::Plugins;
 use Koha::SMS::Providers;
+
+my $messaging_options = C4::Members::Messaging::GetMessagingOptions();
 
 my $input = CGI->new;
 my %data;
@@ -462,6 +465,57 @@ if (    ( $op eq 'edit_form' || $op eq 'cud-insert' || $op eq 'cud-save' || $op 
 {
     unless ( $newdata{'dateexpiry'} ) {
         $newdata{'dateexpiry'} = $category->get_expiry_date( $newdata{dateenrolled} ) if $category;
+    }
+}
+
+# do this for both save and insert  if EnhancedMessagingPreferences and simple-messaging is used
+if ( $op eq 'cud-insert' || $op eq 'cud-save') {
+    # if simplified form is to be used we add the params here
+    if (C4::Context->preference('EnhancedMessagingPreferences') and $input->param('setting_messaging_prefs')) {
+      if ( defined $input->param('simple-messaging') && $input->param('simple-messaging') eq 'yes') {
+          if (defined $input->param ('messaging-simple-radios')) {
+              my %whichActionsToTickUsingSimpleMessaging = map { $_ => 1 } (split /\|/, C4::Context->preference('whichActionsToTickUsingSimpleMessaging')); # split the string to array and then convert to hash to use keys for easy checking
+              if ($input->param('messaging-simple-radios') eq 'sms') {
+                  foreach my $messaging_option (@{$messaging_options})
+                  {
+                    if ($whichActionsToTickUsingSimpleMessaging{$messaging_option->{'message_name'}}) {
+                        $input->param($messaging_option->{'message_attribute_id'}, "sms");
+
+                    }
+                  }
+
+              }
+              elsif ($input->param('messaging-simple-radios') eq 'email') {
+                  # set all types to email
+                  foreach my $messaging_option (@{$messaging_options})
+                  {
+                    if ($whichActionsToTickUsingSimpleMessaging{$messaging_option->{'message_name'}}) {
+                         $input->param($messaging_option->{'message_attribute_id'}, "email");
+                    }
+                  }
+              }
+              elsif (($input->param('messaging-simple-radios') eq 'SmsAndEmail')) {
+                   # set all types to email and sms
+                  foreach my $messaging_option (@{$messaging_options})
+                  {
+                    if ($whichActionsToTickUsingSimpleMessaging{$messaging_option->{'message_name'}}) {
+                      $input->param($messaging_option->{'message_attribute_id'}, "sms", "email");
+                    }
+                  }
+              }
+              elsif (($input->param('messaging-simple-radios') eq 'paper')) {
+                   # set all types to do not notify
+                  foreach my $messaging_option (@{$messaging_options})
+                  {
+                    if ($whichActionsToTickUsingSimpleMessaging{$messaging_option->{'message_name'}}) {
+                      $input->delete($messaging_option->{'message_attribute_id'});
+                    }
+                  }
+              }
+          }
+      }
+      delete $newdata{"messaging-simple-radios"};
+      delete $newdata{"simple-messaging"};
     }
 }
 